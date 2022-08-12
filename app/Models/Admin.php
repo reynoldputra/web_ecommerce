49917d1a4ac2a74693adb47fe0eaebbb8266fa83
reynoldputra17 <reynoldputra1@gmail.com>
2022-08-12 21:02:20 +0700

    finishing model admin, transaksi, stattrans, bank

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -10,4 +10,8 @@ class Admin extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function transaksi(){
+        return $this->hasMany(Transaksi::class);
+    }
 }
--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Transaksi extends Model
 {
     use HasFactory;
+    
+    protected $guarded = ['id'];
+    protected $with = ['admin', 'cart', 'status_transaksi', 'bank'];
+
+    public function admin(){
+        return $this->belongsTo(Admin::class);
+    }
+
+    public function cart(){
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function status_transaksi(){
+        return $this->belongsTo(StatusTransaksi::class);
+    }
+
+    public function bank(){
+        return $this->belongsTo(Bank::class);
+    }
 }
