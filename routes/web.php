b534f64f3ede05c6785e838e69c2a0ffd6fefd1c
ahnafms <ahnafmusyaffa47@gmail.com>
2022-08-23 06:47:14 +0700

    total_harga di cart

--- routes/web.php
+++ routes/web.php
@@ -21,23 +21,12 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/',function(){
     return view('welcome');
 });
-
-Route::get('/product',function(){
-    return view('user.content.product');
-});
-Route::get('/shop',function(){
-    return view('user.content.shopping-cart');
-});
-Route::get('/success',function(){
-    return view('user.content.success');
-});
-
 Auth::routes();
 
 Route::group(["prefix"=>'user', 'middleware'=>['isUser','auth']], function(){
     Route::get('/home', [UserController::class, 'index'])->name('user.index');
     Route::get('/product/{id}', [UserController::class, 'product'])->name('user.product');
-    Route::delete('/deleteCart', [UserController::class, 'deleteCart'])->name('user.deleteCart');
+    Route::delete('deleteCart', [UserController::class, 'deleteCart'])->name('user.deleteCart');
     Route::post('/addToCart', [UserController::class, 'addToCart'])->name('user.addToCart');
     Route::get('/shopping-cart', [UserController::class, 'shoppingcart'])->name('user.shoppingCart');
     Route::put('/checkout', [UserController::class, 'checkout'])->name('user.checkout');
